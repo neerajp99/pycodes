6e526d2bbb1821baf19ceb6903cbe2a9b3be645c
Neeraj Pandey <neerajp1999@gmail.com>
2015-12-22 00:49:14 +0530

    gci task

--- task.py
+++ task.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-'''
+"""
 Simple Python program
 which asks for a Question
-'''
+"""
 
 # Main Function
-def main ():	
+def main ():
 	num = int(input("Question: Enter a number between 1 - 20: "))
 
 	# prints a statement based on a query
